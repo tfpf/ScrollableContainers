fc718e043ba57f50af59bbdd4369b9a8cc26258c
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-01-27 15:12:52 +0530

    Updated examples.

--- examples.py
+++ examples.py
@@ -40,6 +40,29 @@ def single_widget():
 
 ###############################################################################
 
+def entry_form_multiple():
+    root = tk.Tk()
+    root.title('`ScrollableFrameTk` Demo')
+
+    labels = ('Name', 'Age', 'City', 'PIN', 'Occupation', 'Company', 'Profession')
+
+    topframe = ScrollableFrameTk(root)
+    for (i, label) in enumerate(labels):
+        tk.Label(topframe.frame, text=label).grid(row=i, column=0, padx=10, pady=15)
+        tk.Entry(topframe.frame).grid(row=i, column=1, padx=10, pady=15)
+
+    bottomframe = ScrollableFrameTk(root)
+    for (i, label) in enumerate(reversed(labels)):
+        tk.Label(bottomframe.frame, text=label).grid(row=i, column=0, padx=10, pady=15)
+        tk.Entry(bottomframe.frame).grid(row=i, column=1, padx=10, pady=15)
+
+    topframe.pack(expand=True, fill=tk.BOTH, side=tk.LEFT)
+    bottomframe.pack(expand=True, fill=tk.BOTH, side=tk.RIGHT)
+    root.mainloop()
+
+###############################################################################
+
 if __name__ == '__main__':
     grid_of_widgets()
     single_widget()
+    entry_form_multiple()
