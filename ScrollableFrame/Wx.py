00d5fb96486f4532865d1f2fe6ea6c79a099bf8f
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-01-30 15:55:21 +0530

    Added scrollable panel for wxPython.

---
+++ ScrollableFrame/Wx.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3 -B
+
+import wx
+import wx.lib.scrolledpanel as scrolled
+
+__all__ = ['ScrollablePanelWx']
+
+###############################################################################
+
+class ScrollablePanelWx(scrolled.ScrolledPanel):
+    '''
+Container with horizontal and vertical scrolling capabilities. Widgets must be
+added to its `panel` attribute.
+    '''
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # According to the documentation, a sizer is required to calculate the
+        # minimum virtual size of the panel.
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        self.panel = wx.Panel(self)
+        vbox.Add(self.panel, flag=wx.ALIGN_CENTRE | wx.EXPAND)
+        self.SetSizer(vbox)
