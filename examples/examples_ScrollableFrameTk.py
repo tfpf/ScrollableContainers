733c93c54ca2cbf52ea62fddb52be93d2261f39a
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-01-31 17:28:48 +0530

    Named variables logically.

--- examples/examples_ScrollableFrameTk.py
+++ examples/examples_ScrollableFrameTk.py
@@ -46,18 +46,18 @@ def entry_form_multiple():
 
     labels = ('Name', 'Age', 'City', 'PIN', 'Occupation', 'Company', 'Profession')
 
-    topframe = ScrollableFrameTk(root)
+    leftframe = ScrollableFrameTk(root)
     for (i, label) in enumerate(labels):
-        tk.Label(topframe.frame, text=label).grid(row=i, column=0, padx=10, pady=15)
-        tk.Entry(topframe.frame).grid(row=i, column=1, padx=10, pady=15)
+        tk.Label(leftframe.frame, text=label).grid(row=i, column=0, padx=10, pady=15)
+        tk.Entry(leftframe.frame).grid(row=i, column=1, padx=10, pady=15)
 
-    bottomframe = ScrollableFrameTk(root)
+    rightframe = ScrollableFrameTk(root)
     for (i, label) in enumerate(reversed(labels)):
-        tk.Label(bottomframe.frame, text=label).grid(row=i, column=0, padx=10, pady=15)
-        tk.Entry(bottomframe.frame).grid(row=i, column=1, padx=10, pady=15)
+        tk.Label(rightframe.frame, text=label).grid(row=i, column=0, padx=10, pady=15)
+        tk.Entry(rightframe.frame).grid(row=i, column=1, padx=10, pady=15)
 
-    topframe.pack(expand=True, fill=tk.BOTH, side=tk.LEFT)
-    bottomframe.pack(expand=True, fill=tk.BOTH, side=tk.RIGHT)
+    leftframe.pack(expand=True, fill=tk.BOTH, side=tk.LEFT)
+    rightframe.pack(expand=True, fill=tk.BOTH, side=tk.RIGHT)
     root.mainloop()
 
 ###############################################################################
