5fbffdb3b3bc471d8c28f20c9801b93a914e71dd
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-09-18 10:18:17 +0530

    Do not error out if GUI libraries are missing. (#1)
    Errors are postponed from the time of importing `ScrollableContainers` to the time the user attempts to access `ScrollableContainers.MissingLibrary`.

--- src/ScrollableContainers/__init__.py
+++ src/ScrollableContainers/__init__.py
@@ -1,5 +1,6 @@
-from .Qt5 import *
-from .Qt6 import *
-from .Tk import *
-from .Wx import *
+for module in ('.Qt5', '.Qt6', '.Tk', '.Wx'):
+    try:
+        exec(f'from {module} import *')
+    except ImportError:
+        pass
 del Qt5, Qt6, Tk, Wx
