40ea7e5243497eeea3f4f632cee22054acdeac31
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-04-06 14:11:12 +0530

    Fixed invalid module names.
    Except the main module, `ScrollableContainers`. I have already registered it on PyPI, so this invalid name will have to stay.

--- src/ScrollableContainers/__init__.py
+++ src/ScrollableContainers/__init__.py
@@ -2,10 +2,10 @@ import contextlib
 import importlib
 
 with contextlib.suppress(ImportError):
-    from ScrollableContainers.Qt5 import ScrollableAreaQt5
+    from ScrollableContainers._qt5 import ScrollableAreaQt5
 with contextlib.suppress(ImportError):
-    from ScrollableContainers.Qt6 import ScrollableAreaQt6
+    from ScrollableContainers._qt6 import ScrollableAreaQt6
 with contextlib.suppress(ImportError):
-    from ScrollableContainers.Tk import ScrollableFrameTk
+    from ScrollableContainers._tk import ScrollableFrameTk
 with contextlib.suppress(ImportError):
-    from ScrollableContainers.Wx import ScrollablePanelWx
+    from ScrollableContainers._wx import ScrollablePanelWx
