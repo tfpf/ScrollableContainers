5c1d164e214cc4a0b4340633e9cb8b913f9be735
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-09-07 13:24:20 +0530

    Formatted

--- src/ScrollableContainers/_tk.py
+++ src/ScrollableContainers/_tk.py
@@ -46,7 +46,9 @@ class ScrollableFrameTk(ttk.Frame):
         self._window = self._canvas.create_window((0, 0), window=self._frame, anchor=tk.NW)
         self._frame.bind("<Configure>", self._configure_viewport_implicit)
         self._frame.bind("<Motion>", self._peek_scrollbars)
-        self._configure_viewport_implicit_wrapper_id = self._frame.bind("<Expose>", self._configure_viewport_implicit_wrapper)
+        self._configure_viewport_implicit_wrapper_id = self._frame.bind(
+            "<Expose>", self._configure_viewport_implicit_wrapper
+        )
 
         # Initially, the vertical scrollbar is a hair below its topmost
         # position. Move it to said position. No harm in doing the equivalent
