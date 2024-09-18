86f97e624bdb274e556a817796affbe7f82124dc
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-09-18 21:51:01 +0530

    Made `contextlib` private in `ScrollableContainers` namespace

--- src/ScrollableContainers/__init__.py
+++ src/ScrollableContainers/__init__.py
@@ -1,10 +1,10 @@
-import contextlib
+import contextlib as _contextlib
 
-with contextlib.suppress(ImportError):
+with _contextlib.suppress(ImportError):
     from ScrollableContainers._qt5 import ScrollableAreaQt5
-with contextlib.suppress(ImportError):
+with _contextlib.suppress(ImportError):
     from ScrollableContainers._qt6 import ScrollableAreaQt6
-with contextlib.suppress(ImportError):
+with _contextlib.suppress(ImportError):
     from ScrollableContainers._tk import ScrollableFrameTk
-with contextlib.suppress(ImportError):
+with _contextlib.suppress(ImportError):
     from ScrollableContainers._wx import ScrollablePanelWx
