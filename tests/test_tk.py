5d148ab3acc739dc96c590a64fed4a49901dfe08
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-03-10 01:32:06 +0530

    Unit tests skeleton

---
+++ tests/test_tk.py
@@ -0,0 +1,10 @@
+import unittest
+import tkinter as tk
+from ScrollableContainers import ScrollableFrameTk
+
+class TestTk(unittest.TestCase):
+    def setUp(self):
+        self.scrollable_frame = ScrollableFrameTk()
+
+    def test_trivial(self):
+        assert True
